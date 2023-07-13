fc213a3e2faf6a51942983ed164a306a839567de
T0655 <138738962+T0655@users.noreply.github.com>
2023-07-13 13:29:23 +0900

    AppMain.cpp制作（３）
    ヘッダーファイルの制作完了

--- Match3/Match3/inputControl.h
+++ Match3/Match3/inputControl.h
@@ -3,3 +3,21 @@
 /******************
 * マクロ定義
 *******************/
+
+/******************
+* 型定義
+******************/
+
+/*********************
+* プロトタイプ宣言
+**********************/
+
+void Input_Initialize(void);
+void Input_Update(void);
+int Input_Escape(void);
+
+int GetOldKey(int key);
+int GetNowKey(int key);
+int GetKeyFlg(int key);
+int GetMousePositionX(void);
+int GetMousePositionY(void);
